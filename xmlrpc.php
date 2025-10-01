34ab817188e79bf1a6d65be0da20a367f694c4ba
Anantara-Skin <anantara.skin@outlook.com>
2025-10-01 08:22:25 +0100

    Add files via upload

---
+++ xmlrpc.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://anantara.africa</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://anantara.africa/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://anantara.africa/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://anantara.africa/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://anantara.africa/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://anantara.africa/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
